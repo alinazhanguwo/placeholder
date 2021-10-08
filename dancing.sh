72a1325e649d96d776ed4ecd939c730e331a41b8
Alina Zhang <alina.li.zhang@gmail.com>
2021-10-08 16:02:00 -0400

    Update dancing.sh

--- dancing.sh
+++ dancing.sh
@@ -1,24 +1,18 @@
-#!/bin/bash
+# Useful references:
 #
-# Script to keep mouse pointer moving so that, for example, Suspend to RAM timeout does not occur.
-# 
-# The mouse pointer will move around its current position on the screen, i.e. around any position
-# on the screen where you place the pointer. However, if you prefer it to move around the centre
-# of the screen then change mousemove_relative to mousemove in the xdotool command below.
+# https://superuser.com/questions/992511/emulate-a-keyboard-button-via-the-command-line
+# https://ss64.com/vb/sendkeys.html
+# https://social.technet.microsoft.com/Forums/windowsserver/en-US/96b339e2-e9da-4802-a66d-be619aeb21ac/execute-function-one-time-in-every-10-mins-in-windows-powershell?forum=winserverpowershell
+# https://learn-powershell.net/2013/02/08/powershell-and-events-object-events/
 #
-# Set LENGTH to 0 if you do not want the mouse pointer to actually move.
-# Set LENGTH to 1 if you want the mouse pointer to move just a tiny fraction.
-# Set LENGTH to e.g. 100 if you want to see more easily the mouse pointer move.
-LENGTH=100
-#
-# Set DELAY to the desired number of seconds between each move of the mouse pointer.
-DELAY=5
-#
-while true
-do
-    for ANGLE in 0 90 180 270
-    do
-        xdotool mousemove_relative --polar $ANGLE $LENGTH
-        sleep $DELAY
-    done
-done
+# Future enhancements - use events rather than an infinite loop
+$wsh = New-Object -ComObject WScript.Shell
+while (1) {
+  # Send Shift+F15 - this is the least intrusive key combination I can think of and is also used as default by:
+  # http://www.zhornsoftware.co.uk/caffeine/
+  # Unfortunately the above triggers a malware alert on Sophos so I needed to find a native solution - hence this script...
+  $Pos = [System.Windows.Forms.Cursor]::Position
+  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X) + 100), $Pos.Y)
+  $wsh.SendKeys('+{F1}')
+  Start-Sleep -seconds 59
+}
