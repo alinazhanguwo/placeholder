32e2cd6520b8ee3de2f97e07b6a82779466c1279
Alina Zhang <lizhanguwo@gmail.com>
2022-05-11 14:09:48 -0400

    Update word_cloud_plot.py

--- word_cloud_plot.py
+++ word_cloud_plot.py
@@ -18,3 +18,20 @@ wordcloud = WordCloud(width=3000, height=2000, random_state=42, background_color
                       collocations=False, stopwords=STOPWORDS).generate(text)
 # plot
 plot_cloud(wordcloud)
+
+# bought_name[:15]
+# ['85% Lean Ground Beef',
+#  'Organic Apple Slices',
+#  'Apple Honeycrisp Organic',
+#  'Mexican Coffee',
+#  'Lactose Free Fat Free Milk',
+#  'Seedless Red Grapes',
+#  'Large Pineapple Chunks',
+#  'Vanilla Coffee Concentrate',
+#  'Large Alfresco Eggs',
+#  'Blood Oranges',
+#  'Uncured Genoa Salami',
+#  'Mexican Finely Shredded Cheese',
+#  'Organic Refried Black Beans',
+#  'Organic Jasmine Rice',
+#  'Whipped Cream Cheese']
